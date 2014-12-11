a2646378f5c1128e7eff8d08b6674ce16a943e52
mariosg <mariosge90@gmail.com>
2014-12-11 16:32:55 -0500

    Done with Home page + countdown till conference

--- Final/Home.aspx
+++ Final/Home.aspx
@@ -5,8 +5,15 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
     <br />
     <br />
-    <div>World Congress CS-IT Conferences 2015</div>
-    <div>For 25 years the CS-IT Conference has served as a beacon of knowledge and learning, as well as ingenuity. Experts in their varying fields join us to give riveting lectures on the latest in the tech world.</div>
+    <div class="main_frame">
+        <h1>Welcome to the World Congress CS-IT Conferences 2015</h1>
+        <p>For 25 years the CS-IT Conference has served as a beacon of knowledge and learning, as well as ingenuity. 
+            Experts in their varying fields join us to give riveting lectures on the latest in the tech world.</p>
+        <br /><br />
+        <div class="countdown">
+            <asp:Label ID="DaysUntilConference" runat="server" Font-Size="XX-Large" BackColor="White" BorderColor="#FF3300" BorderStyle="Dashed" Height="40px" Font-Bold="True"></asp:Label>
+        </div>
+    </div>
 
 </asp:Content>
 
--- Final/Home.aspx.cs
+++ Final/Home.aspx.cs
@@ -9,6 +9,30 @@ public partial class _GIAbout : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (DaysUntilConference != null)
+        {
+            int daysUntilc = theDaysUntilConference();
+            switch (daysUntilc)
+            {
+                case 0:
+                    DaysUntilConference.Text = "Conference starts Today ;)!";
+                    break;
+                case 1:
+                    DaysUntilConference.Text = "Tomorrow is the Conference!";
+                    break;
+                default:
+                    DaysUntilConference.Text = string.Format(
+                        "There are only {0} days left!", daysUntilc);
+                    break;
+            }
+        }
+    }
+    private int theDaysUntilConference()
+    {
+        DateTime conference = new DateTime(DateTime.Today.Year, 7, 19);
+        if (DateTime.Today > conference)
+            conference = conference.AddYears(1);
+        TimeSpan ts = conference - DateTime.Today;
+        return ts.Days;
     }
 }
